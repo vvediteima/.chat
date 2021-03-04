6d7cc4cf44f0a3ae7ed9a9105057a0ac0a7c5d85
vvediteima <54665155+vvediteima@users.noreply.github.com>
2021-03-04 14:05:32 +0300

    Update msgsend.php

--- msgsend.php
+++ msgsend.php
@@ -1,5 +1,5 @@
 <?php
-$user=trim(shell_exec('echo $USERNAME'));
+$user=trim(shell_exec('echo $USER'));
 $req=system("cat /home/$user/.chat/senddata.txt");
 $req=explode(";",$req);
 $k=file_get_contents("http://3.120.174.176/clientserver/?act=send&cid=".$req[0]."&uid=".$req[1]."&text=".rawurlencode(trim($req[2]))."");
