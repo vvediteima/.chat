ec9b04f114ee231c8a38452eccd0d921718bca82
vvediteima <54665155+vvediteima@users.noreply.github.com>
2021-03-04 14:02:40 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,11 +1,12 @@
 <?php
+$user=trim(shell_exec('echo $USERNAME'));
 $cid=$argv[1];
 $uid=$argv[2];
 while (1) {
 system("clear");
 echo "Message: ";
 $text=trim(fgets(STDIN));
-system("echo '$cid;$uid;$text' > ~/.chat/senddata.txt");
-system("php ~/.chat/msgsend.php");
+system("echo '$cid;$uid;$text' > /home/$user/.chat/senddata.txt");
+system("php /home/$user/.chat/msgsend.php");
 }
 ?>
