6aca2b777c5ae1cc66bb81cb262ac8aa4a23f404
vvediteima <54665155+vvediteima@users.noreply.github.com>
2021-03-04 13:17:26 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,25 @@
+<?php
+echo "Type 'j' to join or 'c' to create: ";
+$act=trim(fgets(STDIN));
+if ($act=='c') {
+    $cid=file_get_contents("http://3.120.174.176/clientserver/?act=new");
+    $uid=1;
+}
+else if ($act=='j') {
+    echo "Key In ChatId: ";
+    $cid=trim(fgets(STDIN));
+    $uid=file_get_contents("http://3.120.174.176/clientserver/?act=join&cid=$cid");
+}
+system("mate-terminal -e 'php /home/vvediteima/.chat/send.php $cid $uid'");
+$old='';
+while (1) {
+///////////
+$new=file_get_contents("http://3.120.174.176/clientserver/?act=show&cid=$cid");
+if ($old!=$new) {
+$old=$new;
+system("clear");
+echo $old;
+}
+///////////
+}
+?>
